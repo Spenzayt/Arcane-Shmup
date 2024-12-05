1b5a58d445bbf0f6bb3439d117ff21c5840e206b
Spenzayt <75225186+Spenzayt@users.noreply.github.com>
2024-12-05 12:57:04 +0100

    Parallax

--- Shoot's em up - Zaun/main.cpp
+++ Shoot's em up - Zaun/main.cpp
@@ -1,26 +1,35 @@
 #include <SFML/Graphics.hpp>
-#include "Classes.hpp"
-#include "Animation & Images.hpp"
-#include <windows.h>
-#include <thread>
-#include <chrono>
-#include <vector>
-#include <string>
+#include <iostream>
+#include "parallax.hpp"
 
+using namespace std;
 using namespace sf;
 
 int main() {
-    game.init();
-    game.initAnimations();
-    while (game.window.isOpen()) {
+    RenderWindow window(VideoMode(1920, 1080), "Effet de Parallaxe", Style::Fullscreen);
+
+    ParallaxBackground background1("../assets/backgrounds/ground-zaun.png", 150.0f, 530, 1, 0.6);
+    ParallaxBackground background2("../assets/backgrounds/background-zaun.jpeg", 20.0f, -1700, 2, 2);
+
+    Clock clock;
+    while (window.isOpen()) {
+        Time deltaTime = clock.restart();
+
         Event event;
-        while (game.window.pollEvent(event)) {
-            if (event.type == Event::Closed)
-                game.window.close();
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed) {
+                window.close();
+            }
         }
-        game.Command();
-        game.window.clear();
-        game.printWindow();
-        game.window.display();
+
+        background1.update(deltaTime.asSeconds());
+        background2.update(deltaTime.asSeconds());
+
+        window.clear();
+        background2.draw(window);
+        background1.draw(window);
+        window.display();
     }
+
+    return 0;
 }
