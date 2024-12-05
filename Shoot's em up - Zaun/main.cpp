1f00ff43628b4a84f11be971f02cb9ea6bc73cba
Spenzayt <75225186+Spenzayt@users.noreply.github.com>
2024-12-05 10:37:53 +0100

    test

--- Shoot's em up - Zaun/main.cpp
+++ Shoot's em up - Zaun/main.cpp
@@ -1,8 +1,4 @@
 #include <SFML/Graphics.hpp>
-<<<<<<< Updated upstream
-#include <SFML/Audio.hpp>
-=======
->>>>>>> Stashed changes
 #include "Classes.hpp"
 #include "Animation & Images.hpp"
 #include <windows.h>
@@ -19,22 +15,12 @@ int main() {
     while (game.window.isOpen()) {
         Event event;
         while (game.window.pollEvent(event)) {
-<<<<<<< Updated upstream
             if (event.type == Event::Closed)
-=======
-            if (event.type == sf::Event::Closed)
->>>>>>> Stashed changes
                 game.window.close();
         }
         game.Command();
         game.window.clear();
         game.printWindow();
         game.window.display();
-<<<<<<< Updated upstream
-        game.window.clear();
-
-
-=======
->>>>>>> Stashed changes
     }
 }
