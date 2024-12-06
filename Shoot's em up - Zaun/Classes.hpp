fc8d988a0698a6bb54c8c8389faa666d5ad9e2ea
Quentin LOPES <quentin.lopes@orange.fr>
2024-12-06 13:53:54 +0100

    sedfse

--- Shoot's em up - Zaun/Classes.hpp
+++ Shoot's em up - Zaun/Classes.hpp
@@ -1,69 +1,13 @@
 ﻿#include <iostream>
 #include <SFML/Graphics.hpp>
-
-class Game {
-
-public : 
-
-	sf::RenderWindow window;
-	sf::Texture ekko_walk_texture;
-	sf::Sprite ekko_walk_sprite;
-	sf::Vector2i ekko_anim;
-
-
-	void init() {
-		window.create(sf::VideoMode(1920, 1080), "Chapitre 1, Zaun : La bataille des nations");
-		window.setFramerateLimit(60);
-	}
-
-	void initAnimations() {
-		if (!ekko_walk_texture.loadFromFile("Ekko_walk_128_V42.png")) {
-			std::cout << "ekko est pas chargé bro" << std::endl << std::endl; // Erreur si le fichier est introuvable
-		}
-		ekko_walk_texture.setSmooth(true);
-		ekko_walk_sprite.setTexture(ekko_walk_texture);
-		ekko_walk_sprite.setTextureRect(sf::IntRect(128, 0, 128, 128));
-		ekko_walk_sprite.setPosition(225, 100);
-	}
-
-	void printWindow() {
-		if (ekko_anim.x * 128 >= ekko_walk_texture.getSize().x) // boucle qui permet de revenir a la premiere slide de l'anim
-			ekko_anim.x = 0;
-		// ici, ce code permet de creer l'animation Idle du personnage
-		ekko_walk_sprite.setTextureRect(sf::IntRect(ekko_anim.x * 128, 0, 128, 128));
-		window.draw(ekko_walk_sprite);
-	}
-
-	void Command() {
-
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
-		{
-			// left key is pressed: move our character
-			ekko_walk_sprite.move(-10.f, 0.f);
-		}
-
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-		{
-			// left key is pressed: move our character
-			ekko_walk_sprite.move(10.f, 0.f);
-		}
-
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
-		{
-			// left key is pressed: move our character
-			ekko_walk_sprite.move(0.f, -10.f);
-		}
-
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-		{
-			// left key is pressed: move our character
-			ekko_walk_sprite.move(0.f, 10.f);
-		}
-	}
-}; Game game;
+#include <thread>
+#include <chrono>
+#include <vector>
+#include <string>
+#include <windows.h>
 
 class Entities {
-protected : 
+protected:
 	int E_coordX = 0;
 	int E_coordY = 0;
 	int E_health = 0;
@@ -72,23 +16,299 @@ protected :
 	bool E_isHit = false;
 	bool E_isDying = false;
 	bool E_isDead = false;
-	std::string name;
+	std::string E_name;
 
-public : 
+public:
 
-	Entities(int h, std::string n) : E_health(h), name(n) {}
+	Entities(bool A) : E_health(), E_coordX(), E_coordY(), E_isAlive(A) {}
 
 	~Entities() {
-		std::cout << name << " est détruit";
+		std::cout << E_name << " est détruit";
+	}
+
+	virtual int losePV(int damage) {
+		E_health -= damage;
+		if (E_health <= 0) {
+			E_health = 0;
+			E_isAlive = false;
+		}
+		return E_health;
+	}
+	virtual int getHealth() {
+		return E_health;
+	}
+	virtual int getCoordX() {
+		return E_coordX;
+	}
+	virtual int setCoordX(int X) {
+		E_coordX += X;
+		return E_coordX;
+	}
+	virtual int getCoordY() {
+		return E_coordY;
+	}
+	virtual int setCoordY(int Y) {
+		E_coordY += Y;
+		return E_coordY;
+	}
+	virtual std::string getName() {
+		return E_name;
+	}
+	virtual bool getAlive() {
+		return E_isAlive;
+	}
+	virtual int heal() {
+		E_health += 30;
+		return E_health;
+	}
+	virtual int setHealth(int pv) {
+		E_health = pv;
+		return E_health;
+	}
+	virtual int HealthReset(int pv) {
+		E_health = pv;
+		return E_health;
+	}
+	virtual bool LifeReset() {
+		E_isAlive = true;
+		return E_isAlive;
 	}
 };
 
-class Character {
-public : 
+class Character : public Entities {
+protected:
+	std::string c_name;
+	int c_coordX;
+	int	c_coordY;
+	int c_health;
+	bool c_isAlive = true;
+
+public:
+	Character(std::string n, int CX, int CY, int h) : c_name(n), c_coordX(CX), c_coordY(CY), c_health(h), Entities(true) {}
+
+	int losePV(int damage) override {
+		c_health -= damage;
+		if (c_health <= 0) {
+			c_health = 0;
+			c_isAlive = false;
+		}
+		return c_health;
+	}
+	int getHealth() override {
+		return c_health;
+	}
+	int getCoordX() override {
+		return c_coordX;
+	}
+	int setCoordX(int X) override {
+		c_coordX += X;
+		return c_coordX;
+	}
+	int getCoordY() override {
+		return c_coordY;
+	}
+	int setCoordY(int Y) override {
+		c_coordY += Y;
+		return c_coordY;
+	}
+	std::string getName() override {
+		return c_name;
+	}
+	bool getAlive() override {
+		return c_isAlive;
+	}
+	int heal() override {
+		c_health++;
+		return c_health;
+	}
+	int setHealth(int pv) override {
+		c_health = pv;
+		return c_health;
+	}
+	int HealthReset(int pv) override {
+		c_health = pv;
+		return c_health;
+	}
+	bool LifeReset() {
+		c_isAlive = true;
+		return c_isAlive;
+	}
+}; 
+
+class Enemies : public Entities {
+protected:
+	std::string e_name;
+	int e_coordX, e_coordY;
+	int e_health;
+	bool e_isAlive = true;
+
+
+public:
+	Enemies(std::string n, int CX, int CY, int h) : e_name(n), e_coordX(CX), e_coordY(CY), e_health(h), Entities(true) {}
+
+	int losePV(int damage) override {
+		e_health -= damage;
+		if (e_health <= 0) {
+			e_health = 0;
+			e_isAlive = false;
+		}
+		return e_health;
+	}
+	int getHealth() override {
+		return e_health;
+	}
+	int getCoordX() override {
+		return e_coordX;
+	}
+	int setCoordX(int X) override {
+		e_coordX += X;
+		return e_coordX;
+	}
+	int getCoordY() override {
+		return e_coordY;
+	}
+	int setCoordY(int Y) override {
+		e_coordY += Y;
+		return e_coordY;
+	}
+	std::string getName() override {
+		return e_name;
+	}
+	bool getAlive() override {
+		return e_isAlive;
+	}
+	int heal() override {
+		e_health++;
+		return e_health;
+	}
+	int setHealth(int pv) override {
+		e_health = pv;
+		return e_health;
+	}
+	int HealthReset(int pv) override {
+		e_health = pv;
+		return e_health;
+	}
+	bool LifeReset() {
+		e_isAlive = true;
+		return e_isAlive;
+	}
+}; 
+
+
+class Ekko : public Character {
+public:
+	Ekko() : Character("Ekko", 225, 625, 500) {}
+
 
 };
 
-class Enemies {
 
-};
+class Game {
+public:
 
+	sf::RenderWindow window;
+	sf::Texture ekko_walk_texture;
+	sf::Sprite ekko_walk_sprite;
+	sf::Vector2i ekko_anim;
+
+
+	sf::Texture ekko_Attack_texture;
+	sf::Sprite ekko_Attack_sprite;
+	sf::Vector2i ekko_anim_Attack;
+
+	struct Ekko {
+		bool isAttacking = false;
+		bool ekko_anim_isAttacking = false;
+		bool isHealing = false;
+		bool isHit = false;
+		bool ekko_anim_isHit = false;
+		bool isDying = false;
+		bool ekko_anim_isDying = false;
+		bool printBody = false;
+		int countAnimAtk = 0;
+		int countAnimHeal = 0;
+		int countAnimHit = 0;
+		int countAnimDeath = 0;
+		int DeathCount = 0;
+	}; Ekko ekko_S;
+
+
+
+	void init() {
+		window.create(sf::VideoMode(1920, 1080), "Zaun : La bataille des nations");
+		window.setFramerateLimit(60);
+	}
+
+	void initAnimations() {
+		if (!ekko_walk_texture.loadFromFile("C:\\Users\\quent\\source\\repos\\Arcane-Shoot-s-em-up\\Shoot's em up - Zaun\\assets\\characters\\ekko\\Ekko_walk_128_V42.png")) {
+			std::cout << "ekko est pas chargé bro" << std::endl << std::endl; // Erreur si le fichier est introuvable
+		}
+		ekko_walk_texture.setSmooth(true);
+		ekko_walk_sprite.setTexture(ekko_walk_texture);
+		ekko_walk_sprite.setTextureRect(sf::IntRect(128, 0, 128, 128));
+		ekko_walk_sprite.setPosition(225, 100);
+		/////////////////
+		if (!ekko_Attack_texture.loadFromFile("C:\\Users\\quent\\source\\repos\\Arcane-Shoot-s-em-up\\Shoot's em up - Zaun\\assets\\characters\\ekko\\Ekko_Attack_128.png")) {
+			std::cout << "ekkoAttack est pas chargé bro" << std::endl << std::endl;
+		}
+		ekko_Attack_texture.setSmooth(true);
+
+		ekko_Attack_sprite.setTexture(ekko_Attack_texture);
+		ekko_Attack_sprite.setTextureRect(sf::IntRect(128, 0, 128, 128));
+		ekko_Attack_sprite.setPosition(225, 100);
+	}
+
+	void printWindow() {
+		if (!ekko_S.ekko_anim_isAttacking) {
+			if (ekko_anim.x * 128 >= ekko_walk_texture.getSize().x) // boucle qui permet de revenir a la premiere slide de l'anim
+				ekko_anim.x = 0;
+			// ici, ce code permet de creer l'animation de marche du personnage
+			ekko_walk_sprite.setTextureRect(sf::IntRect(ekko_anim.x * 128, 0, 128, 128));
+			window.draw(ekko_walk_sprite);
+			//////////////////////////////
+		}
+
+		if (ekko_S.ekko_anim_isAttacking) {
+			if (ekko_anim_Attack.x * 128 >= ekko_Attack_texture.getSize().x)
+				ekko_anim_Attack.x = 0;
+
+			ekko_Attack_sprite.setTextureRect(sf::IntRect(ekko_anim_Attack.x * 128, 0, 128, 128));
+			window.draw(ekko_Attack_sprite);
+		}
+		//////////////////////////////
+	}
+
+	void Command() {
+
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+		{
+			ekko_S.ekko_anim_isAttacking = true;
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
+		{
+			//ekko_walk_sprite.setPosition(Char_Class += 10, 10);
+			ekko_walk_sprite.move(-10.f, 0.f);
+			ekko_Attack_sprite.move(-10.f, 0.f);
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+		{
+			ekko_walk_sprite.move(10.f, 0.f);
+			ekko_Attack_sprite.move(10.f, 0.f);
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
+		{
+			ekko_walk_sprite.move(0.f, -10.f);
+			ekko_Attack_sprite.move(0.f, -10.f);
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+		{
+			ekko_walk_sprite.move(0.f, 10.f);
+			ekko_Attack_sprite.move(0.f, 10.f);
+		}
+	}
+}; Game game;
--- Shoot's em up - Zaun/main.cpp
+++ Shoot's em up - Zaun/main.cpp
@@ -1,48 +1,60 @@
-#include "Classes.hpp"
-#include "Animation & Images.hpp"
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
-#include <windows.h>
-#include <thread>
-#include <chrono>
-#include <vector>
-#include <string>
+﻿#include <SFML/Graphics.hpp>
 #include <iostream>
+#include "parallax.hpp"
 #include "Classes.hpp"
-#include "Animation & Images.hpp"
-#include <thread>
-#include <chrono>
-#include <vector>
-#include <string>
-#include <windows.h>
+
+using namespace std;
+using namespace sf;
 
 int main() {
+
+    //ParallaxBackground background1("assets/backgrounds/ground-zaun.png", 150.0f, 530, 1, 0.6);
+    //ParallaxBackground background2("assets/backgrounds/background-zaun.jpg", 20.0f, -1700, 2, 2);
+
+    //Clock clock;
+
     game.init();
     game.initAnimations();
-    // Boucle principale
+    auto startTime = chrono::steady_clock::now();
+    auto waitTime = chrono::milliseconds(70);
+    auto startAttTime = chrono::steady_clock::now();
+    auto waitAttTime = chrono::milliseconds(30);
+
     while (game.window.isOpen()) {
-        sf::Event event;
+        //Time deltaTime = clock.restart();
+        Event event;
         while (game.window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
-                game.window.close(); // Fermer la fen�tre
+                game.window.close(); // Fermer la fenêtre
         }
         game.Command();
         game.window.clear();
+        auto nowTime = chrono::steady_clock::now();
+        if (nowTime >= startTime + waitTime) {
+            game.ekko_anim.x++;
+            startTime = nowTime;
+        }
+        auto nowAttTime = chrono::steady_clock::now();
+        if (nowAttTime >= startAttTime + waitAttTime) {
+            game.ekko_anim_Attack.x++;
+            if (game.ekko_S.ekko_anim_isAttacking) game.ekko_S.countAnimAtk++;
+            if (game.ekko_S.countAnimAtk == 12) {
+                game.ekko_S.ekko_anim_isAttacking = false;
+                game.ekko_S.countAnimAtk = 0;
+            }
+            startAttTime = nowAttTime;
+        }
+
         game.printWindow();
+
+        //background1.update(deltaTime.asSeconds());
+        //background2.update(deltaTime.asSeconds());
+
+        //background2.draw(game.window);
+        //background1.draw(window);
+
         game.window.display();
-        background1.update(deltaTime.asSeconds());
-        background2.update(deltaTime.asSeconds());
-
-        window.clear();
-        background2.draw(window);
-        background1.draw(window);
-        window.display();
-        background1.update(deltaTime.asSeconds());
-        background2.update(deltaTime.asSeconds());
-
-        window.clear();
-        background2.draw(window);
-        background1.draw(window);
-        window.display();
     }
+
+    return 0;
 }
