b75aebd44f05bdbbdf67783f4c0c205b5d507020
Spenzayt <75225186+Spenzayt@users.noreply.github.com>
2024-12-07 23:52:12 +0100

    Revert "Merge pull request #4 from Spenzayt/Quentin"
    This reverts commit f6ca87b9ded6dcb86a02c3ff1d1b9efbaafd5511, reversing
    changes made to b4793a93b487aa4bede9f3fe59367e009f1f6d97.

---
+++ Shoot's em up - Zaun/Animation & Images.hpp
@@ -0,0 +1,8 @@
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <windows.h>
+#include <thread>
+#include <chrono>
+#include <vector>
+#include <string>
+
--- Shoot's em up - Zaun/main.cpp
+++ Shoot's em up - Zaun/main.cpp
@@ -1,32 +1,25 @@
 ﻿#include <SFML/Graphics.hpp>
 #include <iostream>
 #include "parallax.hpp"
-#include "menu.hpp"
 #include "Classes.hpp"
+#include "menu.hpp"
 
 using namespace std;
 using namespace sf;
 
-Ekko Ekko_Class;
-
 int mainGame() {
 
-    ParallaxBackground background1("assets/backgrounds/ground-zaunV2.png", 150.0f, 630, 1.1, 1.1);
-    ParallaxBackground background2("assets/backgrounds/background-zaun.jpeg", 20.0f, -1890, 2, 2);
+    ParallaxBackground background1("assets/backgrounds/ground-zaun.png", 150.0f, 410, 1, 0.8);
+    ParallaxBackground background2("assets/backgrounds/background-zaun.jpeg", 20.0f, -1700, 2, 2);
 
     Clock clock;
 
     game.init();
-    Ekko_Class.ekkoInitAnimations();
-    Ekko_Class.bulletInit();
-
+    game.initAnimations();
     auto startTime = chrono::steady_clock::now();
     auto waitTime = chrono::milliseconds(70);
     auto startAttTime = chrono::steady_clock::now();
-    auto waitAttTime = chrono::milliseconds(50);
-    auto startReadyToAttackTime = chrono::steady_clock::now();
-    auto waitReadyToAttackTime = chrono::seconds(1);
-
+    auto waitAttTime = chrono::milliseconds(30);
 
     while (game.window.isOpen()) {
         Time deltaTime = clock.restart();
@@ -35,8 +28,7 @@ int mainGame() {
             if (event.type == sf::Event::Closed)
                 game.window.close(); // Fermer la fen�tre
         }
-
-
+        game.Command();
 #pragma region Background
         background1.update(deltaTime.asSeconds());
         background2.update(deltaTime.asSeconds());
@@ -45,12 +37,7 @@ int mainGame() {
         background1.draw(game.window);
 
 #pragma endregion Background
-
-#pragma region Ekko
-
-        Ekko_Class.ekkoCommand();
-        Ekko_Class.ekkoDontExitFromScreen();
-
+        game.dontExitFromScreen();
         auto nowTime = chrono::steady_clock::now();
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) waitTime = chrono::milliseconds(20);
         else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) waitTime = chrono::milliseconds(20);
@@ -60,40 +47,23 @@ int mainGame() {
             waitTime = chrono::milliseconds(70);
         }
         if (nowTime >= startTime + waitTime) {
-            Ekko_Class.ekko_anim.x++;
+            game.ekko_anim.x++;
             startTime = nowTime;
         }
-        if (Ekko_Class.ekko_S.ekko_anim_isAttacking) {
+        if (game.ekko_S.ekko_anim_isAttacking) {
             auto nowAttTime = chrono::steady_clock::now();
             if (nowAttTime >= startAttTime + waitAttTime) {
-                Ekko_Class.ekko_S.countAnimAtk++;
-                Ekko_Class.ekko_anim_Auto_Attack.x++;
-                if (Ekko_Class.ekko_S.countAnimAtk == 1) {
-                    Ekko_Class.bullets.push_back(sf::CircleShape());
-                    Ekko_Class.bullets.back().setFillColor(sf::Color::Transparent);
-                    Ekko_Class.bullets.back().setRadius(10);
-                    Ekko_Class.bullets.back().setPosition(Ekko_Class.ekko_Auto_Attack_sprite.getPosition().x + 128, Ekko_Class.ekko_Auto_Attack_sprite.getPosition().y + 32);
-                }
-                if (Ekko_Class.ekko_S.countAnimAtk == 9) {
-                    Ekko_Class.ekko_S.countAnimAtk = 0;
-                    Ekko_Class.ekko_S.ekko_anim_isAttacking = false;
+                game.ekko_S.countAnimAtk++;
+                game.ekko_anim_Attack.x++;
+                if (game.ekko_S.countAnimAtk == 12) {
+                    game.ekko_S.countAnimAtk = 0;
+                    game.ekko_S.ekko_anim_isAttacking = false;
                 }
                 startAttTime = nowAttTime;
             }
         }
 
-        Ekko_Class.ekkoPrintWindow(game.window);
-
-        for (int i = 0; i < Ekko_Class.bullets.size(); i++) {
-            game.window.draw(Ekko_Class.bullets[i]);
-            Ekko_Class.bullets[i].move(20, 0);
-            Ekko_Class.ekko_Bullet_Auto_Attack_sprite.setPosition(Ekko_Class.bullets[i].getPosition().x -30, Ekko_Class.bullets[i].getPosition().y -6);
-            game.window.draw(Ekko_Class.ekko_Bullet_Auto_Attack_sprite);
-            Ekko_Class.ekko_Bullet_Auto_Attack_sprite.move(20, 0);
-        }
-
-#pragma endregion Ekko
-
+        game.printWindow();
         game.window.display();
     }
 
